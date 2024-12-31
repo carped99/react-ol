e77489ecd2f2e381037069ee69f834870154adb6
tykim <carped99@gmail.com>
2024-12-31 19:47:00 +0900

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 export * from '@src/map/useMap';
 export * from '@src/map/useView';
 
+// Context
+export * from '@src/context/MapContext';
+
+// Observable
 export * from '@src/observable/useObservable';
 export * from '@src/observable/ObservableOptions';
 
-export * from '@src/context/MapContext';
-
+// Layer
 export * from '@src/layer/useGraticuleLayer';
 export * from '@src/layer/useHeatmapLayer';
 export * from '@src/layer/useImageLayer';
@@ -16,14 +19,32 @@ export * from '@src/layer/useVectorLayer';
 export * from '@src/layer/useVectorTileLayer';
 export * from '@src/layer/useWebGLTileLayer';
 
+// Overlay
 export * from '@src/overlay/useTooltipOverlay';
 export * from '@src/overlay/usePopupOverlay';
 export * from '@src/overlay/useShowText';
+
+// Interaction
 export * from '@src/interaction/useInteraction';
-export * from '@src/interaction/useDrawInteraction';
+export * from '@src/interaction/useDblClickDragZoomInteraction';
+export * from '@src/interaction/useDblClickZoomInteraction';
+export * from '@src/interaction/useDragAndDropInteraction';
+export * from '@src/interaction/useDragBoxInteraction';
+export * from '@src/interaction/useDragPanInteraction';
+export * from '@src/interaction/useDragRotateAndZoomInteraction';
+export * from '@src/interaction/useDragRotateInteraction';
 export * from '@src/interaction/useDrawHoleInteraction';
-export * from '@src/interaction/useMapClick';
+export * from '@src/interaction/useDrawInteraction';
+export * from '@src/interaction/useExtentInteraction';
+export * from '@src/interaction/useInteraction';
+export * from '@src/interaction/useKeyboardPanInteraction';
+export * from '@src/interaction/useKeyboardZoomInteraction';
+export * from '@src/interaction/useLinkInteraction';
 export * from '@src/interaction/useModifyInteraction';
+export * from '@src/interaction/useMouseWheelZoomInteraction';
+export * from '@src/interaction/usePinchRotateInteraction';
+export * from '@src/interaction/usePinchZoomInteraction';
+export * from '@src/interaction/usePointerInteraction';
 export * from '@src/interaction/useSelectInteraction';
 export * from '@src/interaction/useSnapInteraction';
 export * from '@src/interaction/useToggleInteraction';
