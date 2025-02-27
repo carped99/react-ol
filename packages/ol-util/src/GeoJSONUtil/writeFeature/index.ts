a1104a4e2af7647df2d12eee170b87f18ba48982
tykim <carped99@gmail.com>
2025-02-28 08:35:22 +0900

    add writeFeatureSanitized

--- packages/ol-util/src/GeoJSONUtil/writeFeature/index.ts
+++ packages/ol-util/src/GeoJSONUtil/writeFeature/index.ts
@@ -11,6 +11,10 @@ export type WriteFeatureOptions<T extends OlGeometry> = {
   options?: WriteOptions;
 };
 
+export type WriteSanitizedFeatureOptions<T extends OlGeometry> = WriteFeatureOptions<T> & {
+  sanitizer?: (feature: OlFeature<T>) => OlFeature<T>;
+};
+
 /**
  * OpenLayers `Feature`를 GeoJSON `Feature`로 변환합니다.
  * @typeParam T - OpenLayers Geometry 타입 파라미터
@@ -60,4 +64,33 @@ export const writeGeometry = <T extends OlGeometry>(geometry: T, options?: Write
   return format.writeGeometryObject(geometry, options?.options) as
     | ToGeoJSONGeometry<T>
     | GeometryCollection<ToGeoJSONGeometry<T>>;
+};
+
+export const writeFeatureSanitized = <T extends OlGeometry>(
+  feature: OlFeature<T>,
+  options?: WriteSanitizedFeatureOptions<T>,
+) => {
+  const sanitizer = options?.sanitizer ?? sanitizeFeature;
+  return writeFeature(sanitizer(feature), options);
+};
+
+export const writeFeaturesSanitized = <T extends OlGeometry>(
+  features: OlFeature<T>[],
+  options?: WriteSanitizedFeatureOptions<T>,
+) => {
+  const sanitizer = options?.sanitizer ?? sanitizeFeature;
+  return writeFeatures(features.map(sanitizer), options);
+};
+
+const sanitizeFeature = <T extends OlGeometry>(feature: OlFeature<T>) => {
+  const cloned = feature.clone();
+  cloned.setId(feature.getId());
+
+  for (const key of cloned.getKeys()) {
+    if (key.startsWith('_') && key.endsWith('_')) {
+      cloned.unset(key);
+    }
+  }
+
+  return cloned;
 };
